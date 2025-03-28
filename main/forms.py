ac973a645420afea61c94e0a46b93ff3a1246c92
KoBalb <artem.kovalyk79@gmail.com  artem.kovalyk79@gmail.com>
2025-03-28 18:54:03 +0300

    переработал

--- main/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import Text
-from django.forms import ModelForm
-
-class TextForm(ModelForm):
-    class Meta:
-        model = Text
-        fields = ['text']
--- main/views.py
+++ main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from .models import Text
 from django.http import HttpResponse
-# Create your views here.
+from .models import UserText
+
 def index(request):
-    text = Text.objects.all()
-    return render(request, 'main/index.html', {"text": text})
+    if request.method == "POST":
+        text = request.POST.get("text")  # Получаем данные из формы
+        if text:  # Если текст не пустой
+            # Сохраняем текст в базе данных
+            UserText.objects.create(text=text)
+        return HttpResponse(f"Текст сохранён: {text}")  # Отправляем ответ
+
+    # Получаем все сохранённые тексты
+    texts = UserText.objects.all()
+    return render(request, "main/index.html", {"texts": texts})
